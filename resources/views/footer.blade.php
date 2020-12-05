5879574cf945f967d09029881c1a562fbc6f2c80
J33T <muhtasim_jeet@hotmail.com>
2020-12-05 23:29:05 +0600

    sidebar, footer, master separate done

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,19 @@
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <footer class="master-footer">
+            <strong>Copyright &copy; 2020 <a href="/">UNIVERSITY</a>.</strong>
+                All rights reserved.
+        </footer>
+      </div>
+    </div>
+</div>
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+<!-- Control sidebar content goes here -->
+</aside>
+<!-- /.control-sidebar -->
+
+
+
--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,5 @@ Route::view('/login', "login");
 
 Route::post('/login', [UserController::class, 'login']);
 
+Route::view('/master', "master");
+
